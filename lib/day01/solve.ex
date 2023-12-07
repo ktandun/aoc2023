7f930eb3c3e066deb04db92450d1eb955cbc65bc
Kenzie Tandun <nudnateiznek@gmail.com>
2023-12-07 21:10:23 +1300

    day 1 part 2

--- lib/day01/solve.ex
+++ lib/day01/solve.ex
@@ -13,20 +13,35 @@ defmodule Day01 do
   def part_two(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&letters_to_digits/1)
-    |> Enum.map(&String.replace(&1, ~r/\D/, ""))
+    |> Enum.map(&find_digits/1)
     |> Enum.map(fn str ->
       {num, _} = Integer.parse(String.first(str) <> String.last(str))
       num
-    |> IO.inspect()
     end)
     |> Enum.sum()
   end
 
-  def letters_to_digits(string) do
-    Regex.scan(~r/(?<one>one)|(?<two>two)|(?<three>three)|(?<four>four)|(?<five>five)|(?<six>six)|(?<seven>seven)|(?<eight>eight)|(?<nine>nine)/, string, capture: :first)
-    |> Enum.flat_map(fn x -> x end)
-    |> Enum.reduce("", fn x -> x end)
+  def find_digits(string) do
+    Regex.scan(
+      ~r/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/,
+      string
+    )
+    |> List.flatten()
+    |> Enum.map(fn x ->
+      case x do
+        "one" -> "1"
+        "two" -> "2"
+        "three" -> "3"
+        "four" -> "4"
+        "five" -> "5"
+        "six" -> "6"
+        "seven" -> "7"
+        "eight" -> "8"
+        "nine" -> "9"
+        _ -> x
+      end
+    end)
+    |> Enum.join("")
   end
 
   def solve() do
@@ -34,14 +49,9 @@ defmodule Day01 do
       File.read(Path.join([System.get_env("HOME"), "advent_of_code_inputs", "day01.part1.txt"]))
 
     part_one(inputOne)
-    part_two("""
-    two1nine
-    eightwothree
-    abcone2threexyz
-    xtwone3four
-    4nineeightseven2
-    zoneight234
-    7pqrstsixteen
-    """)
+    |> IO.inspect()
+
+    part_two(inputOne)
+    |> IO.inspect()
   end
 end
